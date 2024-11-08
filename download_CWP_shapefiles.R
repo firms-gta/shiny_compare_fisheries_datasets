39918811632cca550ef002c87a0c96a2dcb9df4b
BastienIRD <bastien.grasset@ird.fr>
2024-11-08 12:39:18 +0000

    Correct path for downloading NC shapefile_areas

--- download_CWP_shapefiles.R
+++ download_CWP_shapefiles.R
@@ -18,19 +18,12 @@ if (!file.exists(csv_file_in_zip)) {
 
 
 # URL of the first ZIP file to download
-zip_url <- "https://github.com/fdiwg/fdi-codelists/raw/main/global/cwp/cl_nc_areas.zip"
-zip_destfile <- "cl_nc_areas.zip"
-csv_file_in_zip <- "cl_nc_areas.zip" # Specify expected CSV file inside
+csv_url <- "https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/global/firms/gta/cl_nc_areas.csv"
+csv_destfile <- "cl_nc_areas.csv" # Specify expected CSV file inside
 
-# Download and unzip the ZIP file only if the CSV doesn't already exist
-if (!file.exists(csv_file_in_zip)) {
-  if (!file.exists(zip_destfile)) {
-    download.file(zip_url, zip_destfile, method = "auto")
-    message("ZIP file downloaded.")
-  } else {
-    message("ZIP file already exists. Skipping download.")
-  }
-  unzip(zip_destfile, exdir = "data")
+# Download the CSV file if it doesn't already exist
+if (!file.exists(csv_destfile)) {
+  download.file(csv_url, csv_destfile, method = "auto")
 } else {
-  message("CSV file already exists. Skipping download and unzip.")
+  message("CSV file already exists. Skipping download.")
 }
