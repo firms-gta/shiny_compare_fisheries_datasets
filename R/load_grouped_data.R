df55f733136daaf2478af6b65ac4f6f2ac4b08f8
Julien Barde <juldebar@users.noreply.github.com>
2025-02-27 13:25:37 +0100

    Update load_grouped_data.R

--- R/load_grouped_data.R
+++ R/load_grouped_data.R
@@ -7,9 +7,12 @@ if (file.exists(here::here(file.path("data",filename)))) {
 } else {
   flog.info("writting  default parquet dataset (pre_filtered): %s", filename)
   all_codesource_area <- unique(df_sf$codesource_area)
+  
   whole_group_df <- df_sf  %>% 
     dplyr::group_by(codesource_area, dataset, source_authority, species, gear_type, fishing_fleet, year, measurement_unit) %>%   
-    dplyr::summarise(measurement_value = sum(measurement_value, na.rm = TRUE))  %>% ungroup() 
+   dplyr::summarise(measurement_value = sum(measurement_value, na.rm = TRUE))  %>% ungroup() %>% 
+    filter(!is.na(codesource_area))  %>%    
+    dplyr::left_join((df_distinct_geom %>% as_tibble() %>% dplyr::select(-c(geom,ogc_fid))), by=c('codesource_area'))
   
   qs::qsave(whole_group_df, here::here(file.path("data",filename)))
   
